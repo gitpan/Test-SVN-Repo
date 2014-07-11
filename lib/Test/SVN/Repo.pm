5969a48572a60b43b3ab4740ae64f20c1cedb5be
Stephen Thirlwall <sdt@cpan.org>
2014-07-11 01:18:54 +0000

    Import of SDT/Test-SVN-Repo-0.017 from CPAN.
    gitpan-cpan-distribution: Test-SVN-Repo
    gitpan-cpan-version:      0.017
    gitpan-cpan-path:         SDT/Test-SVN-Repo-0.017.tar.gz
    gitpan-cpan-author:       SDT
    gitpan-cpan-maturity:     released

--- lib/Test/SVN/Repo.pm
+++ lib/Test/SVN/Repo.pm
@@ -1,6 +1,6 @@
 package Test::SVN::Repo;
 # ABSTRACT: Subversion repository fixtures for testing
-$Test::SVN::Repo::VERSION = '0.016'; # TRIAL
+$Test::SVN::Repo::VERSION = '0.017';
 use strict;
 use warnings;
 
@@ -260,7 +260,7 @@ Test::SVN::Repo - Subversion repository fixtures for testing
 
 =head1 VERSION
 
-version 0.016
+version 0.017
 
 =head1 SYNOPSIS
 
